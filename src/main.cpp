13c502de216df055db03147d2f44eb1f8a6f9fa6
little_hobbit <rusikarm3@gmail.com>
2023-04-10 18:23:11 +0300

    add sigterm signal handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
+#include <csignal>
 #include <exception>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <string>
 
 #include "app_config.h"
 #include "model/model.h"
 #include "parser/parser.h"
+
+namespace {
+std::function<void()> on_sigterm;  // NOLINT
+
+void signal_handler(int /*sig*/) {
+  if (on_sigterm) {
+    on_sigterm();
+  }
+}
+}  // namespace
 
 inline std::string read_xml(const std::string &path) noexcept {
   std::stringstream in;
@@ -20,12 +32,16 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  std::signal(SIGTERM, signal_handler); // NOLINT
+
   try {
     auto model_description =
         parser::XmlParser().parse(read_xml(config.xml_model_path));
 
     model::Model model;
     model.build_from_description(model_description);
+
+    on_sigterm = [&model] { model.stop(); };
 
     model.start();
 
