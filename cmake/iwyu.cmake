03d272e083c6c203ccbea7e03fd8cc5eeb1191db
little_hobbit <rusikarm3@gmail.com>
2023-10-22 09:59:42 +0300

    CMake: update `CMakeLists.txt`, add coverage targets

--- cmake/iwyu.cmake
+++ cmake/iwyu.cmake
@@ -1,18 +1,17 @@
-if (${RUN_IWYU})
+macro(setup_iwyu_globally)
   find_program(IWYU_EXE 
     NAMES include-what-you-use iwyu
   )
 
-  if(NOT IWYU_EXE)
-    message(STATUS "include-what-you-use not found.")
-  else()
-    message(STATUS "include-what-you-use found: ${IWYU_EXE}")
-    set(IWYU_EXE "${IWYU_EXE}" "-Xiwyu" "--mapping_file=${CMAKE_SOURCE_DIR}/.iwyu.imp" 
-                               "-Xiwyu" "--verbose=1"
-                               "-Xiwyu" "--cxx17ns"
-    )
-
-    set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE ${IWYU_EXE})
-    set(CMAKE_C_INCLUDE_WHAT_YOU_USE ${IWYU_EXE})
+  if (NOT IWYU_EXE)
+    message(ERROR "IWYU not found")
   endif()
-endif()
+
+  message(STATUS "include-what-you-use found: ${IWYU_EXE}")
+  set(IWYU_EXE "${IWYU_EXE}" "-Xiwyu" "--verbose=1"
+                              "-Xiwyu" "--cxx17ns"
+  )
+  set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE ${IWYU_EXE})
+  set(CMAKE_C_INCLUDE_WHAT_YOU_USE ${IWYU_EXE})
+  
+endmacro(setup_iwyu_globally)
